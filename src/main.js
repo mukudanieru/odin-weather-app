7302d8b8b18a7b61faa5ac407bb9acebc9d6ade4
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-01-22 20:41:50 +0800

    testing fetch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
-document.querySelector("body").innerHTML = "Hello!";
+import "./style.css";
+
+const list = document.querySelector("#location");
+
+function getLocation(data) {
+  console.log(data);
+
+  list.replaceChildren();
+
+  data.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = `${item.name}, ${item.region} from ${item.country}.`;
+    list.appendChild(li);
+  });
+}
+
+let timeoutID;
+const search = document.querySelector("#search-weather");
+
+search.addEventListener("input", (e) => {
+  clearTimeout(timeoutID);
+
+  const input = e.target.value;
+  const url = `http://api.weatherapi.com/v1/search.json?key=185f9080f22643cd89d35208262001&q=${input}`;
+
+  timeoutID = setTimeout(() => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error(`HTTP ${response.status}`);
+        return response.json();
+      })
+      .then((data) => {
+        getLocation(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        list.replaceChildren();
+
+        const li = document.createElement("li");
+        li.textContent = "No location found.";
+        list.appendChild(li);
+      });
+  }, 500);
+});
